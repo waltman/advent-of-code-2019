47d255c6762e7693629da91b9d34c2e260e9d124
Walt Mankowski <waltman@pobox.com>
2019-12-01 08:48:40 -0500

    c++ code for day 1

---
+++ day01/rocket_equation.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+const int fuel_req(const int n) {
+    return n/3 - 2;
+}
+
+const int fuel_req_tot(const int n) {
+    int total = 0;
+    int n1 = n;
+    while (true) {
+        int res = fuel_req(n1);
+        if (res > 0) {
+            total += res;
+            n1 = res;
+        } else
+            return total;
+    }
+}
+
+int main(int argc, char *argv[]) {
+    ifstream infile(argv[1]);
+    if (!infile) {
+        perror(argv[1]);
+        exit(1);
+    }
+
+    int total = 0;
+    int total2 = 0;
+    int n;
+    while (infile >> n) {
+        total += fuel_req(n);
+        total2 += fuel_req_tot(n);
+    }
+
+    cout << "part 1: " << total << endl;
+    cout << "part 2: " << total2 << endl;
+}
