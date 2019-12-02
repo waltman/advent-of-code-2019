e26dd5a70bb3f2a24265fd648edd244d99960d0b
Walt Mankowski (macbook pro) <waltman@pobox.com>
2019-12-02 06:18:40 -0500

    refactored day2 code to solve parts 1 and 2

--- day02/1202_program_alarm.py
+++ day02/1202_program_alarm.py
@@ -1,25 +1,35 @@
 #!/usr/bin/env python3
 from sys import argv
+import copy
+
+def run_intcode(pgm_init, noun, verb):
+    pgm = copy.deepcopy(pgm_init)
+    pgm[1] = noun
+    pgm[2] = verb
+    i = 0
+    while True:
+        if pgm[i] == 1:
+            pgm[pgm[i+3]] = pgm[pgm[i+1]] + pgm[pgm[i+2]]
+        elif pgm[i] == 2:
+            pgm[pgm[i+3]] = pgm[pgm[i+1]] * pgm[pgm[i+2]]
+        elif pgm[i] == 99:
+            return pgm[0]
+            break
+        else:
+            print(f'Unknown opcode of {pgm[i]} at position {i}')
+            print(pgm)
+            break
+        i += 4
+    
 
 filename = argv[1]
 with open(filename) as f:
     pgm = [int(x) for x in f.readline().rstrip().split(",")]
 
-i = 0
-pgm[1] = 12
-pgm[2] = 2
-while True:
-    if pgm[i] == 1:
-        pgm[pgm[i+3]] = pgm[pgm[i+1]] + pgm[pgm[i+2]]
-    elif pgm[i] == 2:
-        pgm[pgm[i+3]] = pgm[pgm[i+1]] * pgm[pgm[i+2]]
-    elif pgm[i] == 99:
-        break
-    else:
-        print(f'Unknown opcode of {pgm[i]} at position {i}')
-        print(pgm)
-        break
-    i += 4
+print('Part 1:', run_intcode(pgm, 12, 2))
 
-print('Part 1:', pgm[0])
-
+for noun in range(100):
+    for verb in range(100):
+        if run_intcode(pgm, noun, verb) == 19690720:
+            print('Part 2:', 100 * noun + verb)
+            
