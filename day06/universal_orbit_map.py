506df0658f4416aadc0585d22d0c5246ef296cb8
Walt Mankowski <waltman@pobox.com>
2019-12-06 08:49:48 -0500

    added code for day 6 part 2

--- day06/universal_orbit_map.py
+++ day06/universal_orbit_map.py
@@ -10,3 +10,4 @@ with open(filename) as f:
         edge = line.split(')')
         G.add_edge(*edge)
 print('Part 1:', sum(nx.algorithms.shortest_path_length(G, 'COM').values()))
+print('Part 2:', len(nx.algorithms.shortest_path(G, 'YOU', 'SAN'))-3)
