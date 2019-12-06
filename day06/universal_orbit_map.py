17a1d0c559a29d1cfc2d61b7f36d3c9c4fa6e81a
Walt Mankowski <waltman@pobox.com>
2019-12-06 08:54:33 -0500

    simplified logic

--- day06/universal_orbit_map.py
+++ day06/universal_orbit_map.py
@@ -6,8 +6,6 @@ G = nx.Graph()
 filename = argv[1]
 with open(filename) as f:
     for line in f:
-        line = line.rstrip()
-        edge = line.split(')')
-        G.add_edge(*edge)
+        G.add_edge(*line.rstrip().split(')'))
 print('Part 1:', sum(nx.algorithms.shortest_path_length(G, 'COM').values()))
 print('Part 2:', len(nx.algorithms.shortest_path(G, 'YOU', 'SAN'))-3)
