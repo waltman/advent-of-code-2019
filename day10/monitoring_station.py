43439eee0aad4b387939ea72a61e82b5d963962b
Walt Mankowski (macbook pro) <waltman@pobox.com>
2019-12-10 09:23:43 -0500

    wip from train

--- day10/monitoring_station.py
+++ day10/monitoring_station.py
@@ -22,34 +22,38 @@ def num_visible(asteroids, i):
     visible = {d for d in dists}
     for d in dists:
         s = set()
-        if d[0] == 0
+        if d[0] == 0:
             div = d[1]
         elif d[1] == 0:
             div = d[0]
         elif d[0] > d[1]:
-            div = gcd(d[0], d[1])
+            div = gcd(abs(d[0]), abs(d[1]))
         else:
-            div = gcd(d[1], d[1])
+            div = gcd(abs(d[1]), abs(d[0]))
         slope = (d[0] / div, d[1] / div)
+        print(f"{d=} {slope=}")
+        for x in range(2, MAX):
+            s.add((d[0] + slope[0]*x, d[1] + slope[1]*x))
                       
-        if d[0] == 0: # blocked row
-            if d[1] > 0:
-                for i in range(d[1]+1, MAX):
-                    s.add((0, i))
-            else:
-                for i in range(d[1]-1, -MAX, -1):
-                    s.add((0, i))
-        if d[1] == 0: # blocked col
-            if d[0] > 0:
-                for i in range(d[0]+1, MAX):
-                    s.add((i, 0))
-            else:
-                for i in range(d[0]-1, -MAX, -1):
-                    s.add((i, 0))
-        for x in range(-6, 7):
-            if x == 0 or x == 1:
-                continue
-            s.add((d[0] * x, d[1] * x))
+        # if d[0] == 0: # blocked row
+        #     if d[1] > 0:
+        #         for i in range(d[1]+1, MAX):
+        #             s.add((0, i))
+        #     else:
+        #         for i in range(d[1]-1, -MAX, -1):
+        #             s.add((0, i))
+        # if d[1] == 0: # blocked col
+        #     if d[0] > 0:
+        #         for i in range(d[0]+1, MAX):
+        #             s.add((i, 0))
+        #     else:
+        #         for i in range(d[0]-1, -MAX, -1):
+        #             s.add((i, 0))
+        # for x in range(-6, 7):
+        #     if x == 0 or x == 1:
+        #         continue
+        #     s.add((d[0] * x, d[1] * x))
+        print(f"{d=} {s=}")
         visible -= s
     print(len(visible), visible)
     return len(visible)
@@ -66,4 +70,5 @@ with open(filename) as f:
                 asteroids.append((row,col))
         row += 1
 
+#print(gcd(3,2))
 print('Part 1:', max([num_visible(asteroids, i) for i in range(len(asteroids))]))
