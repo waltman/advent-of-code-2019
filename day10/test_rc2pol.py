98f54256d0685d032d7dcb6b83040615ae54bc63
Walt Mankowski <waltman@pobox.com>
2019-12-10 22:53:59 -0500

    fixed bug in xy2pol()
    I had the y=0 at the bottom instead of the top so my angles were wrong

--- day10/test_rc2pol.py
+++ day10/test_rc2pol.py
@@ -5,17 +5,17 @@ import math
 # - eucludian distance from the origin
 def xy2pol(x,y):
     print(f"{x=} {y=} {math.atan2(x,y)=:.2f} {math.atan2(y,x)=:.2f}")
-    theta = math.atan2(x,y)
+    theta = math.atan2(x,-y)
     if theta < 0:
         theta = 2 * math.pi + theta
     dist = math.sqrt(x**2 + y**2)
     return theta, dist
 
-print(f"{xy2pol(0,1)=}")
-print(f"{xy2pol(1,1)=}")
-print(f"{xy2pol(1,0)=}")
-print(f"{xy2pol(1,-1)=}")
 print(f"{xy2pol(0,-1)=}")
-print(f"{xy2pol(-1,-1)=}")
-print(f"{xy2pol(-1,0)=}")
+print(f"{xy2pol(1,-1)=}")
+print(f"{xy2pol(1,0)=}")
+print(f"{xy2pol(1,1)=}")
+print(f"{xy2pol(0,1)=}")
 print(f"{xy2pol(-1,1)=}")
+print(f"{xy2pol(-1,0)=}")
+print(f"{xy2pol(-1,-1)=}")
