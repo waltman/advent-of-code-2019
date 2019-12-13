35f3204a74467059d7d1e0a18162eeef0b5d335b
Walt Mankowski <waltman@pobox.com>
2019-12-13 09:37:35 -0500

    draw the initial state of the grid

--- day13/care_package.py
+++ day13/care_package.py
@@ -3,25 +3,47 @@ from sys import argv
 from collections import defaultdict
 from Intcode import Intcode
 
+MAX_COL, MAX_ROW = 44, 23
+
+def draw_grid(grid):
+    tile = {0: ' ',
+            1: '+',
+            2: '#',
+            3: '-',
+            4: 'o'
+        }
+
+    s = ''
+    for r in range(MAX_ROW+1):
+        for c in range(MAX_COL+1):
+            s += tile[grid[r][c]]
+        s += "\n"
+    print(s)
+
 # read in the program
 filename = argv[1]
 with open(filename) as f:
     pgm = defaultdict(int, {i:int(x) for i,x in enumerate(f.readline().rstrip().split(","))})
 
 num_blocks = 0
+grid = [[0 for x in range(MAX_COL+1)] for y in range(MAX_ROW+1)]
+print(len(grid), len(grid[0]))
+        
 vc = Intcode(pgm, 0)
 while True:
-    x = vc.run()
+    c = vc.run()
     if vc.halted:
         break
-    y = vc.run()
+    r = vc.run()
     if vc.halted:
         break
     tile = vc.run()
     if vc.halted:
         break
-    print(x,y,tile)
+    print(r,c,tile)
+    grid[r][c] = int(tile)
     if tile == 2:
         num_blocks += 1
     
 print('Part 1:', num_blocks)
+draw_grid(grid)
