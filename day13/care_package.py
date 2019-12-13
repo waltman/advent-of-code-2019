dc7c8728d7d6eebaa66c5d9438c4eefd7c069d1f
Walt Mankowski <waltman@pobox.com>
2019-12-13 18:46:09 -0500

    finished day 13 part 2
    I thought predict_hit_col() was going to have to be smarter to handle bounces
    off the walls, but it turned out I didn't need it!

--- day13/care_package.py
+++ day13/care_package.py
@@ -20,6 +20,20 @@ def draw_grid(grid):
         s += "\n"
     print(s)
 
+def predict_hit_col(bpos, last_bpos):
+    if last_bpos is None:
+        return 22 # initial position
+
+    drow = bpos[0] - last_bpos[0]
+    dcol = bpos[1] - last_bpos[1]
+
+    if drow < 0: # going up, just follow
+        return bpos[1]
+    elif dcol < 0: # going left
+        return bpos[1] - (22 - bpos[0])
+    else: # going right
+        return bpos[1] + (22 - bpos[0])
+
 # read in the program
 filename = argv[1]
 with open(filename) as f:
@@ -52,6 +66,9 @@ print('Part 2:')
 vc = Intcode(pgm, 0)
 vc.pgm[0] = 2
 blocks = set()
+last_bpos = None
+pcol = 0
+score = 0
 while True:
     c = vc.run()
     if vc.halted:
@@ -62,13 +79,30 @@ while True:
     tile = vc.run()
     if vc.halted:
         break
-    print(r,c,tile)
-    grid[r][c] = int(tile)
+    if c == -1:
+        score = tile
+        print(f'{score=}')
+    else:
+        grid[r][c] = int(tile)
+        
     if tile == 2:
         blocks.add((r,c))
     elif tile == 0 and (r,c) in blocks:
         blocks.remove((r,c))
         print(f'hit block {r},{c}! {len(blocks)} left.')
+    elif tile == 4:
+        bpos = (r,c)
+        hit_col = predict_hit_col(bpos, last_bpos)
+        last_bpos = bpos
 
-    if c == -1: # game started
-        print(f'{len(blocks)} to hit')
+        if pcol > hit_col:
+            vc.input = -1
+        elif pcol < hit_col:
+            vc.input = 1
+        else:
+            vc.input = 0
+            
+    elif tile == 3:
+        pcol = c
+
+print('Part 2:', score)
