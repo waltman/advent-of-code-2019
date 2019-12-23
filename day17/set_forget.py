7a96ca481ecddfc8fc28e7aa524fdb4ca0447acf
Walt Mankowski <waltman@pobox.com>
2019-12-22 22:09:28 -0500

    added code to print out the path

--- day17/set_forget.py
+++ day17/set_forget.py
@@ -21,6 +21,27 @@ def is_intersection(grid,r,c):
             grid[r-1][c] == '#' and
             grid[r+1][c] == '#')
 
+def turn(grid, pos, dir):
+    right = {complex(0,1): complex(-1,0),
+             complex(1,0): complex(0,1),
+             complex(0,-1): complex(1,0),
+             complex(-1,0): complex(0,-1)
+    }
+    left = {complex(0,1): complex(1,0),
+            complex(1,0): complex(0,-1),
+            complex(0,-1): complex(-1,0),
+            complex(-1,0): complex(0,1)
+    }
+
+    l = pos + left[dir]
+    r = pos + right[dir]
+    if grid[int(l.imag)][int(l.real)] == '#':
+        return 'L', left[dir]
+    elif grid[int(r.imag)][int(r.real)] == '#':
+        return 'R', right[dir]
+    else:
+        return '', complex(0,0)
+
 # read in the program
 filename = argv[1]
 with open(filename) as f:
@@ -52,3 +73,34 @@ for r in range(1, ROWS-1):
         if is_intersection(grid, r, c):
             tot += (r-1) * (c-1)
 print('Part 1:', tot)
+
+# find the start point
+for r in range(ROWS):
+    for c in range(COLS):
+        if grid[r][c] == '^':
+            print(r,c)
+            pos = complex(c,r)
+            break
+
+# generate path
+dir = complex(-1,0)
+cmds = ['L']
+dist = 0
+while True:
+    new_pos = pos + dir
+    r = int(new_pos.imag)
+    c = int(new_pos.real)
+    if grid[r][c] == '#':
+        dist += 1
+        pos = new_pos
+    else:
+        cmds.append(str(dist))
+        cmd, new_dir = turn(grid, pos, dir)
+        if cmd == '':
+            break
+        else:
+            cmds.append(cmd)
+            dir = new_dir
+            dist = 0
+print(','.join(cmds))
+
