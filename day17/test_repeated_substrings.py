a24afbcd2a37462462ea5772528d99f0a6a6e8dc
Walt Mankowski <waltman@pobox.com>
2019-12-22 21:05:10 -0500

    more test code. not working.

--- day17/test_repeated_substrings.py
+++ day17/test_repeated_substrings.py
@@ -2,6 +2,7 @@ import re
 
 # test on the example at https://adventofcode.com/2019/day/17
 s='R,8,R,8,R,4,R,4,R,8,L,6,L,2,R,4,R,4,R,8,R,8,R,8,L,6,L,2'
+#s='L,6,L,4,R,12,L,6,R,12,R,12,L,8,L,6,L,4,R,12,L,6,L,10,L,10,L,6,L,6,R,12,R,12,L,8,L,6,L,4,R,12,L,6,L,10,L,10,L,6,L,6,R,12,R,12,L,8,L,6,L,4,R,12,L,6,L,10,L,10,L,6'
 
 def best_match(s):
     max_str = ''
@@ -21,12 +22,16 @@ def make_routine(s, patterns):
     routine = []
     start = 0
     while start < len(s):
-        for i in range(3):
+        found = False
+        for i in range(len(patterns)):
             if re.search(f'^{patterns[i]}', s[start:]):
                 routine.append(name[i])
                 start += len(patterns[i])+1
+                print(f'{s[start:]}')
+                print(f'{routine=}')
+                found = True
                 break
-        if i == 3:
+        if not found:
             print(f"couldn't find a pattern in {s[start:]}")
             break
     return routine
@@ -35,9 +40,21 @@ def make_routine(s, patterns):
 patterns = []
 start = 0
 for _ in range(3):
+    # does it start with a pattern we've already seen?
+    done = False
+    while not done:
+        for p in patterns:
+            if re.search(f'^{p}', s[start:]):
+                start += len(p) + 1
+                break
+        done = True
+    print(s[start:])
     p = best_match(s[start:])
     patterns.append(p)
-    start += len(p)+1
+    print(f'{p=}')
+    print(f'{patterns=}')
 
+#if len(patterns) == 0:
+#    patterns.append('')
 print(patterns)
 print(",".join(make_routine(s, patterns)))
