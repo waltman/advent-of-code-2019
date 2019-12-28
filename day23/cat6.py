f2a0d6071d94fe896dcdac0ef3c091fedf95e7f8
Walt Mankowski <waltman@pobox.com>
2019-12-28 08:36:32 -0500

    first draft of code for 23.1
    wip

---
+++ day23/cat6.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+from sys import argv
+from collections import defaultdict
+from Intcode import Intcode
+
+# read in the program
+filename = argv[1]
+with open(filename) as f:
+    pgm = defaultdict(int, {i:int(x) for i,x in enumerate(f.readline().rstrip().split(","))})
+
+N = 50
+vcs = []
+for i in range(N):
+    vcs.append(Intcode(pgm, i))
+
+inputs = [[] for _ in range(N)]
+
+done = False
+while not done:
+    for i in range(N):
+        resp = vcs[i].run()
+        inputs[i].append(resp)
+        print(i, inputs[i])
+        if len(inputs[i]) == 3:
+            dest = inputs[i][0]
+            x = inputs[i][1]
+            y = inputs[i][2]
+            print(i, dest, x, y)
+            if dest == 255:
+                print('Part 1', dest)
+                done = True
+                break
+            inputs[i].clear()
+            vcs[dest].input.append(x)
+            vcs[dest].input.append(y)
+
