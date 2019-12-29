e660864b8f6a949c31805cc62c73e069cc347d97
Walt Mankowski <waltman@pobox.com>
2019-12-28 20:58:27 -0500

    python code for 24.2

--- day24/planet_of_discord2.py
+++ day24/planet_of_discord2.py
@@ -3,22 +3,49 @@ from sys import argv
 from copy import deepcopy
 
 SIZE = 5
-DEPTH = 13
+MINUTES = 200
+DEPTH = MINUTES+3
 
-def adjacent(row,col):
+def adjacent(row,col,level):
+    up_north = (1,2,level+1)
+    up_east  = (2,3,level+1)
+    up_south = (3,2,level+1)
+    up_west  = (2,1,level+1)
+
+    down_north = [(4,c,level-1) for c in range(SIZE)]
+    down_east  = [(r,0,level-1) for r in range(SIZE)]
+    down_south = [(0,c,level-1) for c in range(SIZE)]
+    down_west  = [(r,4,level-1) for r in range(SIZE)]
+
     adj = []
     # n
-    if row > 0:
-        adj.append((row-1,col))
-    # s
-    if row < SIZE-1:
-        adj.append((row+1,col))
+    if row == 0:
+        adj.append(up_north)
+    elif (row,col) == (3,2):
+        adj += down_north
+    else:
+        adj.append((row-1,col,level))
     # e
-    if col < SIZE-1:
-        adj.append((row,col+1))
+    if col == 4:
+        adj.append(up_east)
+    elif (row,col) == (2,1):
+        adj += down_east
+    else:
+        adj.append((row,col+1,level))
+    # s
+    if row == 4:
+        adj.append(up_south)
+    elif (row,col) == (1,2):
+        adj += down_south
+    else:
+        adj.append((row+1,col,level))
     # w
-    if col > 0:
-        adj.append((row,col-1))
+    if col == 0:
+        adj.append(up_west)
+    elif (row,col) == (2,3):
+        adj += down_west
+    else:
+        adj.append((row,col-1,level))
 
     return adj
 
@@ -33,29 +60,22 @@ def stringify_grid(grid):
         s += '\n'
     return s
 
-def evolve(grid):
-    new_grid = [[' ' for col in range(SIZE)] for row in range(SIZE)]
-    for row in range(SIZE):
-        for col in range(SIZE):
-            adj = adjacent(row, col)
-            cnt = len([(r1,c1) for r1, c1 in adj if grid[r1][c1] == '#'])
-            if grid[row][col] == '#' and cnt != 1:
-                new_grid[row][col] = '.'
-            elif grid[row][col] == '.' and 1 <= cnt <= 2:
-                new_grid[row][col] = '#'
-            else:
-                new_grid[row][col] = grid[row][col]
-    return new_grid
-
-def biodiversity_rating(grid):
-    tot = 0
-    rating = 1
-    for row in range(SIZE):
-        for col in range(SIZE):
-            if grid[row][col] == '#':
-                tot += rating
-            rating *= 2
-    return tot
+def evolve(grids):
+    new_grids = [empty_grid() for _ in range(DEPTH)]
+    for level in range(1, DEPTH-1):
+        for row in range(SIZE):
+            for col in range(SIZE):
+                if (row,col) == (2,2):
+                    continue
+                adj = adjacent(row, col, level)
+                cnt = len([(r1,c1,l1) for (r1,c1,l1) in adj if grids[l1][r1][c1] == '#'])
+                if grids[level][row][col] == '#' and cnt != 1:
+                    new_grids[level][row][col] = '.'
+                elif grids[level][row][col] == '.' and 1 <= cnt <= 2:
+                    new_grids[level][row][col] = '#'
+                else:
+                    new_grids[level][row][col] = grids[level][row][col]
+    return new_grids
 
 def empty_grid():
     return [['.' for col in range(SIZE)] for row in range(SIZE)]
@@ -65,6 +85,7 @@ def print_grids(grids):
         print(f'Depth {i - DEPTH//2}:')
         print(stringify_grid(grids[i]))
 
+# read in the input and initialize the stack of grids
 grids = [empty_grid() for _ in range(DEPTH)]
 grid = []
 filename = argv[1]
@@ -73,17 +94,24 @@ with open(filename) as f:
         line = line.rstrip()
         grid.append(list(line))
 grids[DEPTH//2] = grid
-print_grids(grids)
 
-# seen = set()
-# seen.add(stringify_grid(grid))
-# while True:
-#     new_grid = evolve(grid)
-#     key = stringify_grid(new_grid)
-#     if key in seen:
-#         break
-#     else:
-#         seen.add(key)
-#         grid = new_grid
-# print(key)
-# print('Part 1', biodiversity_rating(new_grid))
+# evolve the bugs for MINUTES minutes
+for minute in range(MINUTES):
+    new_grids = evolve(grids)
+    for level in range(1, DEPTH-1):
+        for row in range(SIZE):
+            for col in range(SIZE):
+                if (row,col) == (2,2):
+                    continue
+                grids[level][row][col] = new_grids[level][row][col]
+
+# count up the bugs
+cnt = 0
+for level in range(1, DEPTH-1):
+    for row in range(SIZE):
+        for col in range(SIZE):
+            if (row,col) == (2,2):
+                continue
+            if grids[level][row][col] == '#':
+                cnt += 1
+print('Part 2:', cnt)
