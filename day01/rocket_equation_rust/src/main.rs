750c88af13ba67fa3e8680bb1072d70a9255efea
Walt Mankowski <waltman@pobox.com>
2020-05-25 11:33:00 -0400

    I learned about unwrap()

--- day01/rocket_equation_rust/src/main.rs
+++ day01/rocket_equation_rust/src/main.rs
@@ -1,6 +1,6 @@
 use std::env;
-// use std::fs::File;
-// use std::io::{self, BufRead, BufReader};
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 
 fn fuel_req(n: i32) -> i32 {
     n/3 - 2
@@ -16,30 +16,20 @@ fn fuel_req_tot(n: i32) -> i32 {
 }
 
 fn main() {
-// fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
     let filename = &args[1];
-    println!("filename: {}", filename);
 
-    // let file = File::open(filename)?;
-    // let reader = BufReader::new(file);
-    // let mut total = 0;
-    // for line in reader.lines() {
-    //     println!("line: {}", line?);
-    //     let foo = line?;
-    //     let n: i32 = match foo.parse() {
-    //         Ok(num) => num,
-    //         Err(_) => continue,
-    //     };
-        
-    //     total += fuel_req(n);
-    // }
-
-    // println!("part 1: {}", total);
-
-    for x in 10..20 {
-        println!("{}: {}", x, fuel_req(x));
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+    let mut total = 0;
+    let mut total2 = 0;
+    for line in reader.lines() {
+        let n: i32 = line.unwrap().parse().unwrap();
+        total += fuel_req(n);
+        total2 += fuel_req_tot(n);
     }
 
-//     Ok(())
+    println!("part 1: {}", total);
+    println!("part 2: {}", total2);
+
 }
