4edfbcb554fb87dec7b61a471f9aff7986e91055
Walt Mankowski <waltman@pobox.com>
2020-05-25 18:31:11 -0400

    changed my error handling to be more idiomatic rust

--- day01/rocket_equation_rust/src/main.rs
+++ day01/rocket_equation_rust/src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::error::Error;
 
 fn fuel_req(n: i32) -> i32 {
     n/3 - 2
@@ -15,16 +16,16 @@ fn fuel_req_tot(n: i32) -> i32 {
     }
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
     let filename = &args[1];
 
-    let file = File::open(filename).unwrap();
+    let file = File::open(filename)?;
     let reader = BufReader::new(file);
     let mut total = 0;
     let mut total2 = 0;
     for line in reader.lines() {
-        let n: i32 = line.unwrap().parse().unwrap();
+        let n: i32 = line?.parse()?;
         total += fuel_req(n);
         total2 += fuel_req_tot(n);
     }
@@ -32,4 +33,5 @@ fn main() {
     println!("part 1: {}", total);
     println!("part 2: {}", total2);
 
+    Ok(())
 }
